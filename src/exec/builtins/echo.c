ee7398d09060f40a710d078d27e0c61a54442378
Eliam <40575511+eliamd@users.noreply.github.com>
2025-02-06 15:29:46 +0100

    chore: Update echo.c to fix -n flag behavior

--- src/exec/builtins/echo.c
+++ src/exec/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mosmont <mosmont@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 11:17:23 by edetoh            #+#    #+#             */
-/*   Updated: 2025/01/31 20:17:28 by mosmont          ###   ########.fr       */
+/*   Updated: 2025/02/06 15:29:40 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,19 @@
 void	echo(t_args *args, char **env)
 {
 	int	new_line;
+	int	n_counter;
 
 	(void)env;
 	new_line = 1;
+	n_counter = 0;
 	args = args->next;
 	while (args)
 	{
-		if (ft_strncmp(args->arg, "-n", 2) == 0)
+		if (ft_strncmp(args->arg, "-n", 2) == 0 && n_counter == 0)
+		{
 			new_line = 0;
+			n_counter++;
+		}
 		else
 		{
 			ft_putstr_fd(args->arg, STDOUT_FILENO);
